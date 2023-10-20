285254f94118988f0362a7ef2cd62ebbe71bc286
zhoaa <40127610+zhoaa@users.noreply.github.com>
2023-10-20 13:50:21 -0400

    Update App.tsx to change style of bar chart
    makes it look a little nicer and showcases the bottom axis

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Chart from "./Chart"
 
 function App() {
-  return <Chart intArray={[1, 2, 3]} axisColour="red" />;
+  return <Chart intArray={[1, 2, 3]} stringArray={["Apples", "Onions", "Starfruits"]} axisColour="red" barColour="red" />;
 }
 
 export default App;
